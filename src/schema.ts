48b89ab23c66784340b37805cdf20ca52bdc557a
jes3rk <joseph.schefer@gmail.com>
2023-11-11 12:40:19 +0100

    refactor schema to a single file

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,48 @@
-import { SchemaComposer } from 'graphql-compose'
-import { BlockTC } from './types/block.type'
+import { SchemaComposer } from "graphql-compose";
 
-export const schemaComposer = new SchemaComposer()
+export const schemaComposer = new SchemaComposer();
+
+export const TransactionTC = schemaComposer.createObjectTC({
+  name: "Transaction",
+  fields: {
+    hash: "String!",
+    size: "Int!",
+    energy_consumption: {
+      type: "Float!",
+      resolve: (source) => source.size * 4.62,
+    },
+  },
+});
+
+export const BlockTC = schemaComposer.createObjectTC({
+  name: "Block",
+  fields: {
+    hash: "String!",
+    size: "Int!",
+    energy_consumption: {
+      type: "Float!",
+      resolve: (source) => {
+        return source.size * 4.62;
+      },
+    },
+    tx: {
+      type: () => [TransactionTC],
+      resolve: () => [],
+    },
+  },
+});
 
 schemaComposer.Query.addFields({
   hello: {
-    type: () => 'String!',
-    resolve: () => 'Hi there, good luck with the assignment!',
+    type: () => "String!",
+    resolve: () => "Hi there, good luck with the assignment!",
   },
   findBlockByHash: {
     type: () => BlockTC,
     resolve: () => ({
-      hash: '1234',
+      hash: "1234",
       size: 1243,
-    })
-  }
-})
+      tx: [],
+    }),
+  },
+});
